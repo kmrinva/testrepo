dfaa0cf40b3933535b050c876d1479ba40e08a90
kmrinva <36515469+kmrinva@users.noreply.github.com>
2021-02-13 11:29:13 -0500

    new files for data loading

---
+++ agency_oracle.sql
@@ -0,0 +1,14 @@
+CREATE TABLE  agency (
+  agencyid number,
+  agencycode VARCHAR2(10) ,
+  agencyname VARCHAR2(2000) ,
+  agencytype VARCHAR2(20)  
+  epacode VARCHAR2(20) ,
+  biacode VARCHAR2(10) ,
+  description VARCHAR2(2000) ,
+  createby VARCHAR2(2000) ,
+  createdate DATETIME ,
+  updateby VARCHAR2(2000) ,
+  updatedate DATETIME ,
+  deleteind CHAR(1) 
+);
